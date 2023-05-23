cb3244d079914c9c4bdd0621948e6ba710885d92
Kenix <antoinebd.pro@gmail.com>
2023-05-23 17:12:20 +0200

    correction

--- js/script.js
+++ js/script.js
@@ -9,25 +9,21 @@ function generer(){
         let prenom = document.createElement("td");
         let promo = document.createElement("td");
         let prog = document.createElement("td");
+        let alternance = document.createElement("td")
         let date = document.createElement("td");
 
         nbcar.textContent = monformulaire.elements["nom"].value;
         date.textContent = monformulaire.elements["prenom"].value;
         catego.textContent = monformulaire.elements["promo"].value;
         siteappli.textContent = monformulaire.elements["prog"].value;
+        alternance.textContent = monformulaire.elements["alternance"].value;
         date.textContent = monformulaire.elements["date"].value;
 
-        nom.classList.add("c1");
-        prenom.classList.add("c2");
-        promo.classList.add("c3");
-        prog.classList.add("c4");
-        date.classList.add("c5");
-
-        newLine.append(nom, prenom, promo, prog, date);
+        newLine.append(nom, prenom, promo, prog, alternance, date);
 
         let memberTab = document.getElementById("memberlist");
 
         memberTab.appendChild(newLine);
     }
-    else {alert("Champs incomplets. Veuillez remplir les champs et cocher l'une des cases.");}
+    else {alert("Champs incomplets. Veuillez remplir les champs.");}
 }
